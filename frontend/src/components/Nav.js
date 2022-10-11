18c4398283e52c936602d115c1c568f878cf8aeb
Jordan-Burgess <alfredjordan93@gmail.com>
2022-10-11 12:19:15 -0600

    Add Nav Links

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function Nav() {
   return (
-    <div>Nav</div>
+    <div>
+        <a href='#'>Profile</a>
+        <a href='#'>Chat</a>
+        <a href='#'>Find Partner</a>
+        <a href='#'>Events</a>
+    </div>
   )
 }
