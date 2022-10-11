5ddcf1d54f54a6691c027a17c12e17180dddd67a
Jordan-Burgess <alfredjordan93@gmail.com>
2022-10-11 10:05:53 -0600

    Create profile model

--- backend/main_app/views.py
+++ backend/main_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import JsonResponse
 
-# Create your views here.
+class Users(View):
+    def get(self, request):
+        data = {
+            "name": "Vaibhav",
+            "age": 20,
+            "hobbies": ["Coding", "Art", "Gaming", "Cricket", "Piano"]
+        }
+        return JsonResponse(data)
