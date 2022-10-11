16d5cbf0fdff3e300e556b5d994db821e21f515b
Jordan-Burgess <alfredjordan93@gmail.com>
2022-10-11 12:51:26 -0600

    Add chat room components

--- frontend/src/components/ChatRoom.js
+++ frontend/src/components/ChatRoom.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import ChatRoomHeader from './ChatRoomHeader'
 
 export default function ChatRoom() {
   return (
-    <div>ChatRoom</div>
+    <div>
+      <ChatRoomHeader/>
+    </div>
   )
 }
---
+++ frontend/src/components/ChatRoomHeader.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function ChatRoomHeader() {
+  return (
+    <div> 
+      <img src='#' alt='profile image'/>
+      <h1>Username - Leaning Language</h1>
+    </div>
+  )
+}
