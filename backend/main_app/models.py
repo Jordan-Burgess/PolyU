3ad501257c78a142b8ffe0a436e93057567e5fab
Jordan-Burgess <alfredjordan93@gmail.com>
2022-10-11 14:58:42 -0600

    Create message and conversation models

--- backend/main_app/models.py
+++ backend/main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
@@ -11,3 +12,14 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+class Conversation(models.Model):
+    room = models.CharField(max_length=500)
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='messages')
+    text = models.TextField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE)
+
+
