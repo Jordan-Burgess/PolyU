eb01689a0f321be48ae8040ec5660c6aa5449b1f
Jordan-Burgess <alfredjordan93@gmail.com>
2022-10-14 20:44:12 -0600

    Update post route to take id

--- backend/main_app/views.py
+++ backend/main_app/views.py
@@ -37,6 +37,15 @@ class UserInfo(APIView):
         profile = ProfileSerializer(self.get_user_profile(id), many=True)
         return JsonResponse({"user": user.data, "profile": profile.data}, safe=False)
 
+    def post(self, request, id):
+        request.data['user'] = id
+        serializer = ProfileSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, safe=False)
+        else:
+            return JsonResponse(serializer.errors)
+
     def put(self, request, id):
         data = Profile.objects.get(user_id=id)
         serializer = ProfileSerializer(data, data=request.data)
