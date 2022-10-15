0c005cf40956cbac6cba02fd8f626b7628010578
Jordan-Burgess <alfredjordan93@gmail.com>
2022-10-15 11:37:00 -0600

    Finish login page css

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -4,6 +4,10 @@ import LoginCredentials from '../components/LoginCredentials'
 export default function Login() {
   return (
       <div className="LoginPage">
+        <div className='Title'>
+          <h1>PolyU</h1>
+          <p>Find and connect with a language exchange partner anywhere in the world. Share tips about your culture and teach each a new language.</p>
+        </div>
         <LoginCredentials />
       </div>
   )
