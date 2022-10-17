dcc834a8238fbe771cc8d5030b11c76cbae68fb5
Jordan-Burgess <alfredjordan93@gmail.com>
2022-10-17 02:59:35 -0600

    Add Settings CSS

--- frontend/src/pages/Settings.js
+++ frontend/src/pages/Settings.js
@@ -1,18 +1,38 @@
+import '../styles/settings.css'
 import {useContext} from 'react'
 import Nav from '../components/Nav'
 import AuthContext from '../components/Auth'
+import { useNavigate } from "react-router-dom";
 
 export default function Settings() {
+  const navigate = useNavigate();
   const {logoutUser, user} = useContext(AuthContext)
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    if (window.confirm("Are you sure you want to Delete your account.\nAll your data will be lost. \nThis action cannot be undone.")){
+      const response = await fetch(`http://localhost:8000/${user.user_id}/`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json"
+        },
+      });
+      logoutUser()
+    }
+  } 
+  
   return (
     
-    <div>Settings
+    <div>
+        <div className='SettingsPage'>
+        <h2>Settings</h2>
         <form onSubmit={logoutUser}>
-          <input type="Submit" value="Log Out"/>
+          <input className='Button LogOut' type="Submit" value="Log Out"/>
         </form>
-        <form>
-          Delete Account
+        <form onSubmit={handleSubmit}>
+          <input className='Button Delete' type="Submit" value="Delete Account"/>
         </form>
+        </div>
         <Nav/>
     </div>
   )
