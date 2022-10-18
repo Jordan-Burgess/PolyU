f8f13fce364b484cb7ea25e8cbf16f5b372cdf56
Jordan-Burgess <alfredjordan93@gmail.com>
2022-10-17 20:15:31 -0600

    Edit Cors origin link

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin: "https://poly-u.netlify.app/",
+        origin: "https://poly-u.netlify.app",
         methods: ["GET", "POST"],
     }
 })
